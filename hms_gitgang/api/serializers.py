f482e930b3295381bddf445af80dbe020a220df4
Nyokong <50559078+Nyokong@users.noreply.github.com>
2024-09-03 00:36:29 +0200

    serializer_fixed

--- hms_gitgang/api/serializers.py
+++ hms_gitgang/api/serializers.py
@@ -34,6 +34,8 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = CustomUser(
             username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
             email=validated_data['email']
         )
         user.set_password(validated_data['password'])
